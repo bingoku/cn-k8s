beeaafa97216b6c748aacfdc42e68a8364c6fa1e
快乐哼唱 <xiake21x@qq.com>
2018-07-26 16:41:03 +0800

    新增自动 pull 阿里云镜像然后修改 tag 的脚本

---
+++ cn-k8s.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+KUBE_VERSION=v1.11.1
+KUBE_PAUSE_VERSION=3.1
+ETCD_VERSION=3.2.18
+DNS_VERSION=1.14.8
+CORE_VERSION=1.1.3
+
+GCR_URL=k8s.gcr.io
+ALIYUN_URL=registry.ap-northeast-1.aliyuncs.com/cn-k8s
+
+images=(kube-proxy-amd64:${KUBE_VERSION}
+kube-scheduler-amd64:${KUBE_VERSION}
+kube-controller-manager-amd64:${KUBE_VERSION}
+kube-apiserver-amd64:${KUBE_VERSION}
+pause:${KUBE_PAUSE_VERSION}
+etcd-amd64:${ETCD_VERSION}
+coredns:$CORE_VERSION)
+
+
+for imageName in ${images[@]} ; do
+  docker pull $ALIYUN_URL/$imageName
+  docker tag  $ALIYUN_URL/$imageName $GCR_URL/$imageName
+  docker rmi $ALIYUN_URL/$imageName
+done
+
+docker images
+
