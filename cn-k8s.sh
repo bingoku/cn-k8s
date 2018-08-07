111caa6d7e7b5e2a3cb7e478ebe321fb2811915c
快乐哼唱 <xiake21x@qq.com>
2018-08-07 17:14:43 +0800

    新增 heapster

--- cn-k8s.sh
+++ cn-k8s.sh
@@ -8,12 +8,15 @@ ETCD_VERSION=3.2.18
 DNS_VERSION=1.14.8
 CORE_VERSION=1.1.3
 DASHBOARD_VERSION=v1.8.3
+HEAPSTER_VERSION=v1.5.4
+
 
 GCR_URL=k8s.gcr.io
 ALIYUN_URL=registry.cn-shenzhen.aliyuncs.com/cn-k8s
 
 images=(kube-proxy-amd64:${KUBE_VERSION}
 kube-scheduler-amd64:${KUBE_VERSION}
+heapster-amd64:${HEAPSTER_VERSION}
 kube-controller-manager-amd64:${KUBE_VERSION}
 kube-apiserver-amd64:${KUBE_VERSION}
 kubernetes-dashboard-amd64:${DASHBOARD_VERSION}
